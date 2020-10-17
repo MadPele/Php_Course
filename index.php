28d4e51e30f068be4ff24c5190a2450946e63d69
Andrzej Derela <andrzejderela@gmail.com>
2020-10-17 19:36:34 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Php Course</title>
+</head>
+<body>
+<?php
+
+$name = "Pele";
+
+echo "Welcome $name" . "<br>";
+
+echo "I like dogs";
+
+?>
+</body>
+</html>
